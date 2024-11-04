ee2eacf31771de24358bb14a47b2c974d8702e0f
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-04 16:32:24 -0500

    Done Exercise 4

--- bin/mad9135_exercise_4.dart
+++ bin/mad9135_exercise_4.dart
@@ -1,5 +1,28 @@
-import 'package:mad9135_exercise_4/mad9135_exercise_4.dart' as mad9135_exercise_4;
+import 'package:mad9135_exercise_4/mad9135_exercise_4.dart'
+    as mad9135_exercise_4;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main(List<String> arguments) {
-  print('Hello world: ${mad9135_exercise_4.calculate()}!');
+void main() async {
+  final url = Uri.parse('https://random-data-api.com/api/v2/users?size=10');
+  try {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // Decode the response to a List of Map objects
+      final List<dynamic> users = jsonDecode(response.body);
+
+      // Loop through and print each user's details
+      for (var user in users) {
+        final uid = user['id'] ?? 'N/A';
+        final firstName = user['first_name'] ?? 'N/A';
+        final lastName = user['last_name'] ?? 'N/A';
+        print('User: ID $uid, Name: $firstName $lastName');
+      }
+    } else {
+      print('Failed to load user data');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
 }
