ff204c47fbbdbc3685f11a1c3c51970a66ba8112
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-04 16:06:30 -0500

    Init dart project

---
+++ bin/mad9135_exercise_4.dart
@@ -0,0 +1,5 @@
+import 'package:mad9135_exercise_4/mad9135_exercise_4.dart' as mad9135_exercise_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mad9135_exercise_4.calculate()}!');
+}
---
+++ test/mad9135_exercise_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:mad9135_exercise_4/mad9135_exercise_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
